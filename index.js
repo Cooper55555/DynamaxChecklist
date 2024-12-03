9cd56ab28957b627c386c258e7e6442b93fe05bf
Cooper55555 <milanvelthaak1@gmail.com>
2024-12-03 19:45:56 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+const checkboxes = document.querySelectorAll('.pokemon-checkbox');
+const numberChange = document.getElementById('numberchange');
+
+const savedCount = localStorage.getItem('checkedCount');
+if (savedCount) {
+    numberChange.textContent = savedCount;
+    checkboxes.forEach((checkbox, index) => {
+        checkbox.checked = savedCount > index;
+    });
+}
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        const checkedCount = Array.from(checkboxes).filter(checkbox => checkbox.checked).length;
+        numberChange.textContent = checkedCount;
+        localStorage.setItem('checkedCount', checkedCount);
+    });
+});
